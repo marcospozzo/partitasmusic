e39d0bec94bb6b5bec3c3ccf633dfc3f1afd8b3e
Marcos Pozzo <marcospozzo@MacBook-Air-de-Marcos.local>
2020-10-12 20:56:55 -0300

    protected views work and partials added

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,16 @@ const router = require('express').Router();
 
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 
-// index Page
+// index
 router.get('/', forwardAuthenticated, (req, res) => res.render('index'));
 
-// login Page
+// artist
+router.get('/artist', ensureAuthenticated, (req, res) => res.render('artist'));
+
+// login
 router.get('/login', forwardAuthenticated, (req, res) => res.render('login'));
 
-// dashboard
+// home
 router.get('/home', ensureAuthenticated, (req, res) =>
   res.render('home', {
     user: req.user,
