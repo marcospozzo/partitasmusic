c308124f47120b34e138c870eab2c439a2a42569
Marcos Pozzo <pozzomarc@gmail.com>
2021-03-18 04:54:16 -0300

    contact form fix

--- models/email/contact.js
+++ models/email/contact.js
@@ -2,7 +2,7 @@ const sendMail = require("./email");
 
 function sendContactForm(name, email, message) {
   const data = {
-    to: "info@partitasmusic.com",
+    to: "partitasmusic@gmail.com",
     subject: `[Form submit] ${name}`,
     text: `Dear Partitas: \n\nThere is a new contact message: \nName: ${name} \nEmail: ${email} \nMessage: ${message} \n\nBest regards.`,
     html: "",
