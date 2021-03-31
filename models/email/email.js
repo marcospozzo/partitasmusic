4befbb6ebb93ea28960cbd0f66a4e3a9fa9188a8
Marcos Pozzo <pozzomarc@gmail.com>
2021-03-31 04:21:42 -0300

    email service back to service gmail instead of host, port and secure

--- models/email/email.js
+++ models/email/email.js
@@ -6,9 +6,7 @@ dotenv.config();
 // async..await is not allowed in global scope, must use a wrapper
 async function email(data) {
   let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    service: "gmail",
     auth: {
       user: process.env.USER_EMAIL,
       pass: process.env.USER_PASSWORD,
