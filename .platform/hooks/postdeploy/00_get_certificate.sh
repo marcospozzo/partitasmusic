938c7f5dd9f6c3ae8309cb3209775482a3e310d4
Marc Pozzo <pozzomarc@gmail.com>
2022-04-01 17:32:37 -0300

    ssl aws env url updated

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo certbot --nginx -n -d partitasmusic.us-east-1.elasticbeanstalk.com -d partitasmusic.com -d www.partitasmusic.com --expand --agree-tos --email pozzomarc@gmail.com
+sudo certbot --nginx -n -d partitasmusic-env.eba-k3urgtmm.us-east-1.elasticbeanstalk.com -d partitasmusic.com -d www.partitasmusic.com --expand --agree-tos --email pozzomarc@gmail.com
