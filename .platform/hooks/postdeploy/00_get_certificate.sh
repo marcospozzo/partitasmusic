ff536ce99447ce64781a3ed00dea1bdf4808250d
Marc Pozzo <pozzomarc@gmail.com>
2022-04-04 12:32:31 -0300

    aws eb commands removed

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-#sudo certbot --nginx -n -d partitasmusic-env.eba-k3urgtmm.us-east-1.elasticbeanstalk.com -d partitasmusic.com -d www.partitasmusic.com --expand --agree-tos --email pozzomarc@gmail.com
