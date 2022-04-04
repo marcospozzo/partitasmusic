62aa69c5104eed075e3bde199c7a1a5e37bb540d
Marc Pozzo <pozzomarc@gmail.com>
2022-04-04 16:39:51 -0300

    certbot command to only aws domain

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo certbot --nginx -n -d partitasmusic-env.eba-k3urgtmm.us-east-1.elasticbeanstalk.com --expand --agree-tos --email pozzomarc@gmail.com
+sudo certbot --nginx -n -d partitasmusic-env.eba-k3urgtmm.us-east-1.elasticbeanstalk.com --agree-tos --email pozzomarc@gmail.com
