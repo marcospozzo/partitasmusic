42da8c1317b4f01fb15b0b9e819ca3d2d41c9c3f
Marc Pozzo <pozzomarc@gmail.com>
2022-09-21 18:53:19 -0300

    scroll to top button added for the entire website after the head closing tag

--- views/assets/js/script.js
+++ views/assets/js/script.js
@@ -157,3 +157,32 @@ $(window).click(function (event) {
 $(document).keyup(function (e) {
   if (e.keyCode === 27) $("#myModal").hide(); // esc key
 });
+
+///// Scroll to top button start
+
+// Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 500 ||
+    document.documentElement.scrollTop > 500
+  ) {
+    mybutton.style.display = "flex";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+///// Scroll to top button end
