780904261c60612e93be435e165b71b74eb8396b
Marc Pozzo <pozzomarc@gmail.com>
2023-02-01 19:23:05 -0300

    ssl fix

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-sudo certbot --nginx --non-interactive -d partitasmusic.com -d www.partitasmusic.com --keep-until-expiring --agree-tos --email pozzomarc@gmail.com
-sudo certbot --nginx --non-interactive -d www.partitasmusic.com --keep-until-expiring --agree-tos --email pozzomarc@gmail.com
+sudo certbot --nginx --non-interactive -d partitasmusic.us-east-1.elasticbeanstalk.com -d partitasmusic.com -d www.partitasmusic.com --keep-until-expiring --agree-tos --email pozzomarc@gmail.com
