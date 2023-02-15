2faa603f0d51fd6714416ae76561b6c198482d53
Marc Pozzo <pozzomarc@gmail.com>
2023-02-15 19:13:40 -0300

    ssl workaround

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo certbot --nginx --non-interactive -d partitasmusic.com -d www.partitasmusic.com --keep-until-expiring --agree-tos --email pozzomarc@gmail.com
+sudo certbot --nginx --non-interactive -d partitasmusic.com -d *.partitasmusic.com --keep-until-expiring --agree-tos --email pozzomarc@gmail.com
