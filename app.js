c1a9f38d69846400445ace0ba708d2c9ba5f2b4c
Marcos Pozzo <mpozzo@dataxstream.com>
2024-07-17 12:17:35 -0300

    feat: increase requests limit size

--- app.js
+++ app.js
@@ -36,8 +36,9 @@ app.use(
     ],
   })
 );
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.static("views"));
 app.enable("trust proxy");
 
