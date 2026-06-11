2929734e9660c62986fb5e19e5185e4a0f563310
Marcos Pozzo <mpozzo@dataxstream.com>
2026-06-11 16:08:50 +0200

    fix: set trust proxy to improve handling of forwarded requests

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ app.use(
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.static("views"));
-app.enable("trust proxy");
+app.set("trust proxy", 1);
 
 // session
 app.use(
